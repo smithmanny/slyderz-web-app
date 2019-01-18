c5755a56369ddb3923d23b03e5948ba95d055a0d
Shakhor Smith <shakhorsmith@gmail.com>
2019-01-18 00:16:29 -0500

    Prod build fix?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-const withGraphql = require('next-plugin-graphql');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+      ? require('next/constants') // Get values from `next` package when building locally
+      : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = withGraphql({
-  webpack(config, options) {
-    return config;
-  },
-});
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withGraphql = require('next-plugin-graphql');
+
+  return withGraphql();
+};
