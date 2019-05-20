c7a2b7bb2c70deede04df95eada53d4e1e40f007
Shakhor Smith <shakhorsmith@gmail.com>
2019-05-19 23:09:48 -0400

    added process variables

--- utils/withApollo.js
+++ utils/withApollo.js
@@ -1,14 +1,12 @@
 import ApolloClient from 'apollo-boost';
 import withApollo from 'next-with-apollo';
 
-import configs from '../configs';
-
 function createClient({ headers }) {
   return new ApolloClient({
     uri:
       process.env.NODE_ENV === 'development'
-        ? configs.DEV_URL
-        : configs.GRAPHQL_URL,
+        ? process.env.DEV_URL
+        : process.env.GRAPHQL_URL,
     request: operation => {
       operation.setContext({
         fetchOptions: {
