55d6ad85fcd693e069996e76fb638ddc8c6f828d
Shakhor Smith <shakhorsmith@gmail.com>
2019-05-20 12:47:54 -0400

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-nested-ternary */
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION
-    ? require('next/constants') // Get values from `next` package when building locally
-    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
-
 const withGraphql = require('next-plugin-graphql');
 
 module.exports = withGraphql();
