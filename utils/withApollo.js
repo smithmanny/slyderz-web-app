1d2283cca136f3501317943e9bbaf072cd3aa1e6
Shakhor Smith <shakhorsmith@gmail.com>
2019-05-21 23:43:24 -0400

    add auth

--- utils/withApollo.js
+++ utils/withApollo.js
@@ -14,7 +14,10 @@ function createClient({ headers }) {
         fetchOptions: {
           credentials: 'include'
         },
-        headers
+        headers: {
+          ...headers,
+          Authorization: 'Bearer GTC8OArCuIy4tRt92u3frGSEAQIXtB8Z'
+        }
       });
     }
   });
