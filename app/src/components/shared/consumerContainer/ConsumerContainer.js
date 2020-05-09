c518c7abbb6186d41924de4ea4064fed6413681f
Shakhor <shakhorsmith@gmail.com>
2020-05-08 22:39:55 -0400

    Enable app bar

--- app/src/components/shared/consumerContainer/ConsumerContainer.js
+++ app/src/components/shared/consumerContainer/ConsumerContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 
-// import AppBar from '../../appbar';
+import AppBar from '../../appbar';
 import consumerContainerStyles from './styles';
 
 const ConsumerContainer = ({ children, ...props }) => {
   const classes = consumerContainerStyles();
   return (
     <Container className={classes.container} maxWidth="xl" {...props}>
-      {/* <AppBar /> */}
+      <AppBar />
       {children}
     </Container>
   );
