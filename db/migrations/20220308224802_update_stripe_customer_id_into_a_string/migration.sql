d7ba0e05207a192cf061a040577c98b8ff3dd8a7
Shakhor Smith <shakhorsmith@gmail.com>
2022-08-21 12:28:03 -0400

    update migration

--- db/migrations/20220308224802_update_stripe_customer_id_into_a_string/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "stripeCustomerId" SET DATA TYPE TEXT;
--- db/migrations/20220504025631_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dishId` on the `Section` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Section" DROP COLUMN "dishId";
