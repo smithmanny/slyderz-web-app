b0b742738db211112f07445d7cb580613c60febe
Shakhor Smith <shakhorsmith@gmail.com>
2022-08-21 14:11:02 -0400

    update setting stripe key

--- app/helpers/site.js
+++ app/helpers/site.js
@@ -1,2 +1,10 @@
 export const siteUrl = process.env.VERCEL_URL || "http://localhost:3000"
-export const STRIPE_SECRET = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || ''
+
+function setStripeKey() {
+  if (process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY === undefined || null) {
+    return 'sk_test_PHGdRXFISHoYwFqSlVPfTEh7'
+  }
+
+  return process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
+}
+export const STRIPE_SECRET = setStripeKey();
