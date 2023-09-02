ecd3ffad927f9c069888f643afed96d0021d0c18
Shakhor Smith <shakhorsmith@gmail.com>
2023-09-02 10:54:36 -0400

    add beta footer (#220)

---
+++ app/beta/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Typography from "app/core/components/shared/Typography";
+
+function BetaFooter() {
+  return (
+    <footer>
+      <div style={{ padding: 30, textAlign: "center" }}>
+        <Typography>&#169; Slyderz 2023</Typography>
+      </div>
+    </footer>
+  );
+}
+
+export default BetaFooter;
