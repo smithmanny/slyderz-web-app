f654e78a975542eff533669f61a45d28df363342
Shakhor Smith <shakhors@getamplifylife.com>
2023-09-06 16:41:40 -0400

    add rudderstack

---
+++ app/utils/getRudderstack.ts
@@ -0,0 +1,25 @@
+export const RudderStack = (function() {
+  let instance;
+
+  async function createInstance() {
+    window.rudderanalytics = await import("rudder-sdk-js");
+
+    window.rudderanalytics.load(process.env.NEXT_PUBLIC_POSTHOG_KEY, process.env.NEXT_PUBLIC_POSTHOG_HOST, {
+      integrations: { All: true }, // load call options
+    });
+
+    window.rudderanalytics.ready(() => {
+      console.log("RudderStack loaded...");
+    });
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance()
+      }
+
+      return instance
+    }
+  }
+})();
