1838777275d5c057828b4353439e9554c87f9fbf
Shakhor Smith <shakhors@getamplifylife.com>
2023-09-16 19:01:43 -0400

    Finshed profile description

--- app/onboarding/components/ProfileDescription.tsx
+++ app/onboarding/components/ProfileDescription.tsx
@@ -1,6 +1,9 @@
+import React, { useCallback } from "react";
+
+import { trpc } from "server/utils/trpc";
+
 import Form, { TextField } from "app/core/components/form";
 import Typography from "app/core/components/shared/Typography";
-
 import { AddChefDescription } from "../validations";
 
 interface ProfileDescriptionType {
@@ -8,10 +11,29 @@ interface ProfileDescriptionType {
 }
 
 function ProfileDescription(props: ProfileDescriptionType) {
+  const utils = trpc.useContext();
+
+  const invalidateOnboardingState = useCallback(async () => {
+    await utils.chef.invalidate();
+  }, [utils.chef]);
+
+  const setChefDescription =
+    trpc.onboarding.completeOnboardingDescription.useMutation({
+      onSuccess: invalidateOnboardingState,
+    });
   return (
     <>
       <Typography variant="body2">{props.description}</Typography>
-      <Form schema={AddChefDescription} submitText="Finsh Onboarding">
+      <Form
+        schema={AddChefDescription}
+        submitText="Finsh Onboarding"
+        mutation={{
+          schema: setChefDescription.mutateAsync,
+          toVariables: (values) => ({
+            ...values,
+          }),
+        }}
+      >
         <TextField
           name="description"
           multiline
---
+++ db/migrations/20230916220027_add_description_to_chef/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Chef" ADD COLUMN     "description" TEXT NOT NULL DEFAULT '';
