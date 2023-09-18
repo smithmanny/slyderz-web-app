1bbe890a146aba24e4c0f7ddedcf6c7ee982d175
Shakhor Smith <shakhors@getamplifylife.com>
2023-09-18 11:19:40 -0400

    updates to schema

--- integrations/auth/lucia.ts
+++ integrations/auth/lucia.ts
@@ -2,6 +2,7 @@ import { lucia } from "lucia";
 import { idToken } from "@lucia-auth/tokens";
 import { nextjs } from "lucia/middleware";
 import { prisma } from "@lucia-auth/adapter-prisma";
+import { generateRandomString, isWithinExpiration } from "lucia/utils"; // v2 beta.0
 import db from 'db'
 import "lucia-auth/polyfill/node";
 
@@ -27,10 +28,32 @@ export const auth = lucia({
 
 const EXPIRES_IN = 1000 * 60 * 60 * 2; // 2 hours
 
-const isWithinExpiration = (expiresIn: number) => {
-	const currentTime = new Date().getTime();
-	return currentTime < expiresIn;
-}
+const generateToken = async (userId: string) => {
+	const token = generateRandomString(64); // github uses 128+ for password reset tokens
+
+	// you can optionally invalidate all user tokens
+	// so only a single valid token exists per user
+	// for high security apps
+
+	await db.setToken({
+		id: token,
+		expires: new Date().getTime() + EXPIRES_IN,
+		user_id: userId
+	});
+	return token;
+};
+
+const validateToken = async (token: string) => {
+	const storedToken = await db.getToken(token);
+	if (!storedToken) throw new Error("Invalid token");
+	await db.deleteToken(token);
+	const tokenExpires = Number(storedToken.expires); // bigint => number conversion
+	if (!isWithinExpiration(tokenExpires)) {
+		// should throw a vague error to user (expired *or* invalid token)
+		throw new Error("Expired token");
+	}
+	return storedToken.user_id;
+};
 
 const generateVerificationToken = async (userId: string) => {
 	const storedUserTokens = await db.getTokensByUserId(userId); // github uses 128+ for password reset tokens
@@ -48,21 +71,15 @@ const generateVerificationToken = async (userId: string) => {
 	// you can optionally invalidate all user tokens
 	// so only a single valid token exists per user
 	// for high security apps
-
-	const token = generateRandomString(64);
-	await prismaClient.setToken({
-		id: token,
-		expires: new Date().getTime() + EXPIRES_IN,
-		user_id: userId
-	});
-	return token;
+	const token = await generateToken(userId);
+	return token
 };
 
-export const passwordResetToken = idToken(auth, "password_reset", {
-  expiresIn: 60 * 60, // 1 hour
-});
-export const emailVerificationToken = idToken(auth, "email_verification", {
-  expiresIn: 60 * 60, // 1 hour
-});
+// export const passwordResetToken = idToken(auth, "password_reset", {
+//   expiresIn: 60 * 60, // 1 hour
+// });
+// export const emailVerificationToken = idToken(auth, "email_verification", {
+//   expiresIn: 60 * 60, // 1 hour
+// });
 
 export type Auth = typeof auth;
