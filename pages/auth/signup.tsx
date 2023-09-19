1bc6e254726eea16ab6682efcb766494bb7cf534
Shakhor Smith <shakhors@getamplifylife.com>
2023-09-19 13:06:05 -0400

    redirect signin users

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,8 +1,28 @@
 import { useRouter } from "next/router";
 
+import { auth } from "integrations/auth/lucia";
+
 import ConsumerContainer from "app/core/components/shared/ConsumerContainer";
 import Layout from "app/layouts/Layout";
 import { SignupForm } from "app/auth/components/SignupForm";
+
+export async function getServerSideProps(ctx) {
+  const authRequest = auth.handleRequest(ctx);
+  const { session } = await authRequest.validateUser();
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const SignupPage = () => {
   const router = useRouter();
